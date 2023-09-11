e3905dd168350dfec859c7ecc463e52b18716a14
Saroj <sarojt7475@gmail.com>
2023-09-11 09:14:59 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const arr =[
+{name:’hari’},
+{name: ‘ram’},
+{name: ‘gopal’},4444
+{name:’hari’},5
+{name: ‘gopal’},
+{name: ‘chrish’}
+]
+
+
+
+arr.map((item) => {
+
+})
+
+
+
+// given this list, loop over the array in react and assign unique color to duplicates
+// this is a high difficulty level react test, please try to solve this
